52be93f60f6769c63ea7191449df4ea084eafc08
David Rokhinson <drokhinson@gmail.com>
2025-05-02 08:24:26 -0500

    Add Instrument defs
    Build out classes defining key properties of contracts

---
+++ DRLib/Instrument/InstDefs.cs
@@ -0,0 +1,26 @@
+﻿namespace DRLib.Instrument;
+
+public record Inst;
+
+public interface IExpirable
+{
+    public DateTime ExpDt { get; }
+}
+
+public record FutureChain(string Und) : Inst;
+
+public record Future(string Und, DateTime ExpDt) : FutureChain(Und), IExpirable;
+
+public abstract record Option(string Und, double Strike, DateTime ExpDt, bool IsCall) : Inst;
+
+public abstract record EuropeanOption(string Und, double Strike, DateTime ExpDt, bool IsCall) : Option(Und, Strike, ExpDt, IsCall);
+public record EuroCall(string Und, double Strike, DateTime ExpDt) : EuropeanOption(Und, Strike, ExpDt, true);
+public record EuroPut(string Und, double Strike, DateTime ExpDt) : EuropeanOption(Und, Strike, ExpDt, false);
+
+public abstract record AmericanOption(string Und, double Strike, DateTime ExpDt, bool IsCall) : Option(Und, Strike, ExpDt, IsCall);
+public record AmericanCall(string Und, double Strike, DateTime ExpDt) : AmericanOption(Und, Strike, ExpDt, true);
+public record AmericanPut(string Und, double Strike, DateTime ExpDt) : AmericanOption(Und, Strike, ExpDt, false);
+
+public abstract record DigitalOption(string Und, double Strike, DateTime ExpDt, bool IsCall) : Option(Und, Strike, ExpDt, IsCall);
+public record DigiCall(string Und, double Strike, DateTime ExpDt) : DigitalOption(Und, Strike, ExpDt, true);
+public record DigiPut(string Und, double Strike, DateTime ExpDt) : DigitalOption(Und, Strike, ExpDt, false);
---
+++ DRLib/Instrument/OptionUtils.cs
@@ -0,0 +1,25 @@
+﻿namespace DRLib.Instrument;
+
+public enum OptionType
+{
+    European,
+    American,
+    Digital,
+    Asian,
+}
+
+public static class OptionUtils
+{
+    public static double CalcPayoff(this Option opt, double spot)
+    {
+        var payoff = opt switch {
+            EuroCall or AmericanCall => spot - opt.Strike,
+            EuroPut or AmericanPut => opt.Strike - spot,
+            DigiCall => spot > opt.Strike ? 1 : 0,
+            DigiPut => spot < opt.Strike ? 1 : 0,
+            _ => throw new NotImplementedException()
+        };
+
+        return payoff > 0 ? payoff : 0;
+    }
+}
